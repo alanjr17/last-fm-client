50cc65981dfd43230c81a19d107d0059fdffb5a1
Alan Omar Martínez <alan.martinez.adesis@gmail.com>
2016-01-30 18:42:56 -0600

    feat(app.config.js): prueba de peticion http
    Se crea una vista con el response de una llamada http de prueba

--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -1,6 +1,13 @@
 (function () {
     function config ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('home');
 
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'views/partials/main.html',
+                controller: 'HomeCtrl'
+            });
     }
 
     angular.module('lastFmApp').config(config);
